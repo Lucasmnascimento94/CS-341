4744600c71053a0475f654fad8cd1aba8e7667ed
Lucas Nascimento <lucasmnascimento94@gmail.com>
2025-09-29 10:41:02 -0400

    ..

--- ATMEGA-SCHEMATICS/SNAKE_GAME/program/gridController/StartSetUp/isr.d
+++ ATMEGA-SCHEMATICS/SNAKE_GAME/program/gridController/StartSetUp/isr.d
@@ -1,15 +1,9 @@
 StartSetUp/isr.o: StartSetUp/isr.c StartSetUp/isr.h StartSetUp/start.h \
  StartSetUp/includes.h libC/DataStructures.h StartSetUp/includes.h \
  libC/print.h
-
 StartSetUp/isr.h:
-
 StartSetUp/start.h:
-
 StartSetUp/includes.h:
-
 libC/DataStructures.h:
-
 StartSetUp/includes.h:
-
 libC/print.h:
--- ATMEGA-SCHEMATICS/SNAKE_GAME/program/gridController/libC/print.d
+++ ATMEGA-SCHEMATICS/SNAKE_GAME/program/gridController/libC/print.d
@@ -1,5 +1,3 @@
 libC/print.o: libC/print.c libC/print.h StartSetUp/includes.h
-
 libC/print.h:
-
 StartSetUp/includes.h:
