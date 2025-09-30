4ded0edf98c1007ddc49f644ca9de8c51031b550
Lucas Nascimento <lucasmnascimento94@gmail.com>
2025-09-30 14:34:09 -0400

    .

--- ATMEGA-SCHEMATICS/SNAKE_GAME/program/gridController/StartSetUp/sram.c
+++ ATMEGA-SCHEMATICS/SNAKE_GAME/program/gridController/StartSetUp/sram.c
@@ -6,6 +6,7 @@ void writeByte(char *c, uint32_t address){
     START_SPI;                         // CS low
     for (uint8_t i = 0; i < strlen(c); i++){
         SPDR = (uint8_t)c[i];          // start transfer
+        while(!(SPSR & (1<<SPIF))){}
     }
     STOP_SPI;                          // CS high
 }
