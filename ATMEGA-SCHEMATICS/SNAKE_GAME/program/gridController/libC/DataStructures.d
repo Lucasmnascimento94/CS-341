d963bdaec7591432c49f52c309ef2e3b2a0323e7
My Huynh <mhuynh1524@gmail.com>
2025-10-05 17:37:14 -0400

    Update DataStructures.d

--- ATMEGA-SCHEMATICS/SNAKE_GAME/program/gridController/libC/DataStructures.d
+++ ATMEGA-SCHEMATICS/SNAKE_GAME/program/gridController/libC/DataStructures.d
@@ -1,12 +1,5 @@
 libC/DataStructures.o: libC/DataStructures.c libC/DataStructures.h \
-<<<<<<< Updated upstream
  StartSetUp/includes.h libC/print.h
 libC/DataStructures.h:
 StartSetUp/includes.h:
-libC/print.h:
-=======
- includes.h libC/../startSetUp/uart/uart.h
-libC/DataStructures.h:
-includes.h:
-libC/../startSetUp/uart/uart.h:
->>>>>>> Stashed changes
+libC/print.h
