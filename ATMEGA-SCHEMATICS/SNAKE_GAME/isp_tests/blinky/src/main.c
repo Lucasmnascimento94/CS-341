0e6634a98cdebe8a8f1dcbaaf881b8bd5e4c28ea
Martin Scheele <martin.scheele@proton.me>
2025-10-17 19:13:01 -0400

    8MHz + flash readback "verification"

--- ATMEGA-SCHEMATICS/SNAKE_GAME/isp_tests/blinky/src/main.c
+++ ATMEGA-SCHEMATICS/SNAKE_GAME/isp_tests/blinky/src/main.c
@@ -4,9 +4,7 @@
 int main() {
     DDRD |= _BV(PD0);
     while (1) {
-        PORTD &= ~_BV(PD0);
-        _delay_ms(1000);
-        PORTD |= _BV(PD0);
-        _delay_ms(1000);
+        PORTD ^= _BV(PD0);
+        _delay_ms(500);
     }
 }
