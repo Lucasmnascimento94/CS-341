a18c89c92af7108111c6659c5f7b60d8cb24c05d
Lucas Nascimento <lucasmnascimento94@gmail.com>
2025-10-18 21:40:02 -0400

    .

--- ATMEGA-SCHEMATICS/SNAKE_GAME/program/pyApps/trimHex.py
+++ ATMEGA-SCHEMATICS/SNAKE_GAME/program/pyApps/trimHex.py
@@ -6,38 +6,38 @@ def parseFIle(file, file_out):
     page_array = [[]]
 
     block_count = 0
-    length = ""
-    addr = ""
-    data_type = ""
-    data = ""
-    cc = ""
     byte = ""
     out = open("file_out", "w")
+    full_out = open("trimmed_out", "w")
 
     for s in file:
         s = s.strip()
+        length = "0x"
+        addr = "0x"
+        data_type = "0x"
+        data = ""
+        cc = "0x"
+
         for i in range(1, 3):
             length+=(s[i])
-
         for i in range(3, 7):
             addr+=(s[i])
-
         for i in range(7, 9):
             data_type+=(s[i])
 
         if len(s) > 10:
             for i in range(9, len(s)-2):
                 byte += s[i]
-                data+=(s[i])
                 byte_count += 1
-                
+
                 if(byte_count == 4):
-                    out.write("0x"+byte+", ")
+                    hi_lo = byte[2:4] + byte[0:2]
+                    out.write("0x" + hi_lo.upper() + ", ")
+                    data+=("0x" + hi_lo.upper() + ", ")
                     byte = ""
                     byte_count = 0
                     block_count+=1
                     page_count+=1
-                
                 if(block_count == 8):
                     out.write("\n")
                     block_count = 0
@@ -48,7 +48,11 @@ def parseFIle(file, file_out):
 
         for i in range(len(s)-2, len(s)):
             cc+=(s[i])
+        
+        full_out.write("LL: "+ length + " <> Addr: " + addr + " <> Type: " + data_type + " <> Data: " + data + " <> CheckSum: " + cc + "\n")
+    
     out.close()
+    full_out.close()
     
 def main():
     if len(sys.argv) > 1:
